55cec2b3bd33ae44204608041379adc1b37add3f
bring.beer <folleck@web.de>
2020-06-05 10:40:55 +0200

    first setup

---
+++ app/classes/Calculation.cpp
@@ -0,0 +1,5 @@
+//
+// Created by suppe on 6/5/20.
+//
+
+#include "Calculation.h"
---
+++ app/classes/Calculation.h
@@ -0,0 +1,14 @@
+//
+// Created by suppe on 6/5/20.
+//
+
+#ifndef CALCULATOR_TINF19B3_CALCULATION_H
+#define CALCULATOR_TINF19B3_CALCULATION_H
+
+
+class Calculation {
+
+};
+
+
+#endif //CALCULATOR_TINF19B3_CALCULATION_H
