832a98e9790fe92507631f200e0d9bc826108498
bring.beer <folleck@web.de>
2020-06-12 12:30:53 +0200

    base layout for calculations and exceptions

--- app/classes/Calculation.cpp
+++ app/classes/Calculation.cpp
@@ -3,4 +3,57 @@
 //
 
 #include "Calculation.h"
-#include "cmath"
+#include <cmath>
+
+Calculation::Calculation(std::string operand, double pX, double pY) : _x(pX), _y(pY){
+   Calculation::check(operand);
+}
+
+Calculation::Calculation(std::string operand, double pX) : _x(pX){
+    Calculation::check(operand);
+}
+
+Calculation::~Calculation() {}
+
+double Calculation::sqrt(){
+    return ::std::sqrt(_x);
+}
+double Calculation::addition(){
+    return (_x + _y);
+};
+double Calculation::subtraction(){
+    return (_x - _y);
+}
+double Calculation::multiply(){
+    return (_x * _y);
+}
+double Calculation::division(){
+    return (_x / _y);
+}
+double Calculation::pow(){
+    return ::std::pow(_x, _y);
+}
+void Calculation::checkAndCalculate(std::string operand) {
+    if (operand == "/" || operand == ":"){
+        if (_y == 0){
+            throw std::invalid_argument("Division by Zero is not defined.");
+        }
+        _result = Calculation::division();
+    }
+    if (operand == "^"){
+        if (_y < 0){
+            operand = "/";
+        }
+        _result = Calculation::pow();
+    }
+    if (operand == "R"){
+        if (_x < 0) {
+            throw std::invalid_argument("Sqrt < 0 is not defined.");
+        }
+        _result = Calculation::sqrt();
+    }
+}
+
+double Calculation::getResult() {
+    return _result;
+}
--- app/classes/Calculation.h
+++ app/classes/Calculation.h
@@ -5,11 +5,28 @@
 #ifndef CALCULATOR_TINF19B3_CALCULATION_H
 #define CALCULATOR_TINF19B3_CALCULATION_H
 
+#include <iostream>
 
 class Calculation {
 public:
-    Calculation(double x, double y);
-    Calculation(double  x);
+    Calculation(std::string operand, double pX, double pY);
+    Calculation(std::string operand, double pX);
+    ~Calculation();
+
+    double sqrt();
+    double addition();
+    double subtraction();
+    double multiply();
+    double division();
+    double pow();
+
+    void checkAndCalculate(std::string operand);
+    double getResult();
+
+private:
+    double _result;
+    double _x;
+    double _y;
 };
 
 
