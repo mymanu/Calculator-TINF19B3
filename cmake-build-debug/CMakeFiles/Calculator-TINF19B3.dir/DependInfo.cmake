ad9ba5c0b086c528c4bc5f32ba6284d99364a119
bring.beer <folleck@web.de>
2020-06-12 10:21:57 +0200

    new friday new luck with git

--- cmake-build-debug/CMakeFiles/Calculator-TINF19B3.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/Calculator-TINF19B3.dir/DependInfo.cmake
@@ -4,9 +4,9 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "C:/Users/manue/CLionProjects/Calculator-TINF19B3/app/classes/Calculation.cpp" "C:/Users/manue/CLionProjects/Calculator-TINF19B3/cmake-build-debug/CMakeFiles/Calculator-TINF19B3.dir/app/classes/Calculation.cpp.obj"
-  "C:/Users/manue/CLionProjects/Calculator-TINF19B3/app/classes/Logic.cpp" "C:/Users/manue/CLionProjects/Calculator-TINF19B3/cmake-build-debug/CMakeFiles/Calculator-TINF19B3.dir/app/classes/Logic.cpp.obj"
-  "C:/Users/manue/CLionProjects/Calculator-TINF19B3/main.cpp" "C:/Users/manue/CLionProjects/Calculator-TINF19B3/cmake-build-debug/CMakeFiles/Calculator-TINF19B3.dir/main.cpp.obj"
+  "/home/suppe/dh/Calculator-TINF19B3/app/classes/Calculation.cpp" "/home/suppe/dh/Calculator-TINF19B3/cmake-build-debug/CMakeFiles/Calculator-TINF19B3.dir/app/classes/Calculation.cpp.o"
+  "/home/suppe/dh/Calculator-TINF19B3/app/classes/Logic.cpp" "/home/suppe/dh/Calculator-TINF19B3/cmake-build-debug/CMakeFiles/Calculator-TINF19B3.dir/app/classes/Logic.cpp.o"
+  "/home/suppe/dh/Calculator-TINF19B3/main.cpp" "/home/suppe/dh/Calculator-TINF19B3/cmake-build-debug/CMakeFiles/Calculator-TINF19B3.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
--- cmake-build-debug/CMakeFiles/Calculator-TINF19B3.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/Calculator-TINF19B3.dir/cmake_clean.cmake
@@ -1,11 +1,9 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/Calculator-TINF19B3.dir/app/classes/Calculation.cpp.obj"
-  "CMakeFiles/Calculator-TINF19B3.dir/app/classes/Logic.cpp.obj"
-  "CMakeFiles/Calculator-TINF19B3.dir/main.cpp.obj"
-  "Calculator-TINF19B3.exe"
-  "Calculator-TINF19B3.exe.manifest"
+  "CMakeFiles/Calculator-TINF19B3.dir/app/classes/Calculation.cpp.o"
+  "CMakeFiles/Calculator-TINF19B3.dir/app/classes/Logic.cpp.o"
+  "CMakeFiles/Calculator-TINF19B3.dir/main.cpp.o"
+  "Calculator-TINF19B3"
   "Calculator-TINF19B3.pdb"
-  "libCalculator-TINF19B3.dll.a"
 )
 
 # Per-language clean rules from dependency scanning.
